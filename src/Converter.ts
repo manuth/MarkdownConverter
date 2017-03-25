af0d04fddd6a8b7f1483ee3e3f26c6c67faae77f
Manuel Thalmann <m@nuth.ch>
2017-03-26 00:58:11 +0100

    Added a phantom-call

--- src/Converter.ts
+++ src/Converter.ts
@@ -1,6 +1,10 @@
 import * as FS from 'fs';
+import * as Path from 'path';
+import * as phantomjs from 'phantomjs-prebuilt';
+import * as childprocess from 'child_process';
 import { Base } from "./Core/Base";
 import { Document } from "./Document";
+import { ConversionType } from "./ConversionType";
 
 /**
  * Provides a markdown-converter.
@@ -21,5 +25,35 @@ export class Converter extends Base
     constructor(document : Document)
     {
         super();
+        this.document = document;
+    }
+
+    public Start(conversionType : ConversionType, path : string) : void
+    {
+        let type = this.getValueName(ConversionType, conversionType);
+        let args = [
+            Path.join(__dirname, 'Phantom', 'PDFGenerator.js'),
+            type,
+            this.document.toJSON(),
+            Path.resolve(path)
+        ];
+        var result = childprocess.spawnSync(phantomjs.path, args);
+        console.log(result.stdout.toString());
+    }
+
+    private getValueName(enumObject : any, value : any)
+    {
+        let enumArray = [ ];
+        let enumValues = { };
+        for (var key in enumObject)
+        {
+            enumArray.push(key);
+        }
+        for (var i = (enumArray.length / 2) - 1; i >= 0; i--)
+        {
+            let key = enumArray[i];
+            enumValues[key] = enumArray[(enumArray.length / 2) + i];
+        }
+        return enumValues[value];
     }
 }
