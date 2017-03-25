5361a3430d70bdf209135bf5104515d2aead2c40
Manuel Thalmann <m@nuth.ch>
2017-03-25 23:40:16 +0100

    Declarated conversion-types

---
+++ src/ConversionType.ts
@@ -0,0 +1,18 @@
+/**
+ * Represents a conversion-type.
+ */
+enum ConversionType
+{
+    /**
+     * DocX-conversion.
+     */
+    DocX,
+    /**
+     * HTML-conversion.
+     */
+    HTML,
+    /**
+     * PDF-conversion.
+     */
+    PDF
+}
