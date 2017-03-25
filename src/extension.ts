ea478ffb3783c5dd505150e58fa2266550b1151d
Manuel Thalmann <m@nuth.ch>
2017-03-25 23:41:49 +0100

    Added a test.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DateTimeFormatter } from './Core/DateTimeFormatter';
+import { Document } from './Document';
+import { Footer, Header } from './Section';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,9 +21,57 @@ export function activate(context: vscode.ExtensionContext)
     let disposable = vscode.commands.registerCommand('markdownConverter.convert', async () =>
     {
         // The code you place here will be executed every time your command is executed
+        // let markdownDoc = getMarkdownDoc();
+
+        // if (markdownDoc)
+        // {
+        //     let doc : Document;
+        //     if (markdownDoc.isUntitled)
+        //     {
+        //         doc = new Document();
+        //         doc.Content = markdownDoc.getText();
+        //     }
+        //     else
+        //     {
+        //         if (markdownDoc.isDirty)
+        //         {
+        //             markdownDoc.save();
+        //         }
+        //         doc = new Document(markdownDoc.fileName);
+        //     }
+        // }
+        // else
+        // {
+        //     throw Error('No markdown-file found.');
+        // }
+        let doc : Document = new Document();
+        doc.SpecialHeaders[3] = new Header('10cm', 'Seite 10');
+        doc.StyleSheets.push('../../styles.css');
+        doc.Content = '# Hello World\n{{ PageNumber }}\n{{ CreationDate }}';
+
+        console.log(doc.toJSON());
     });
 
     context.subscriptions.push(disposable);
+
+    /**
+     * Tries to find a markdown-file
+     */
+    function getMarkdownDoc() : vscode.TextDocument
+    {
+        if (vscode.window.activeTextEditor.document.languageId == 'markdown')
+        {
+            return vscode.window.activeTextEditor.document;
+        }
+        for (let i = 0; i < vscode.window.visibleTextEditors.length; i++)
+        {
+            if (vscode.window.visibleTextEditors[i].document.languageId == 'markdown')
+            {
+                return vscode.window.visibleTextEditors[i].document;
+            }
+        }
+        return null;
+    }
 }
 
 // this method is called when your extension is deactivated
