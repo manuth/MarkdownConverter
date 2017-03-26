b0a20247576165081cad42f273da4a7603850252
Manuel Thalmann <m@nuth.ch>
2017-03-26 16:58:01 +0200

    Finished the Phantom-Script

--- src/Phantom/PDFGenerator.ts
+++ src/Phantom/PDFGenerator.ts
@@ -1,48 +1,273 @@
+/* Preventing TypeScript's "name not found"-errors */
+var document = document;
 var phantom = phantom;
+
+/* Importing phantom's modules */
 var system = require('system');
 var webpage = require('webpage');
 
-
 try
 {
-    Main(system.args);
+    Main(system.args.slice(1));
 }
 catch (e)
 {
     if (e instanceof Error)
     {
-        console.log(e);
+        system.stderr.writeLine(e);
         phantom.exit(1);
     }
 }
 
 {
     /**
-     * 
+     * Starts the rendering-process.
      * @param args 
      */
     function Main(args) : void
     {
-        var page = webpage.create();
-        
-        page.setContent(args.HTML, null);
-        page.onLoadFinished = function(status)
-        {
-            RenderPage(page);
-        }
-    }
+        /**
+         * The file-type of the resulting file.
+         */
+        var type : string;
 
-    /**
-     * 
-     * @param page 
-     */
-    function RenderPage(page)
-    {
-        page.paperSize = {
-            header:
+        /**
+         * The document to render.
+         */
+        var doc;
+
+        /**
+         * The path to save the rendered file to.
+         */
+        var destination : string;
+
+        if (args.length >= 3)
+        {
+            var page = webpage.create();
+            type = args[0];
+            doc = JSON.parse(args[1]);
+            destination = args[2];
+            
+            page.onLoadFinished = function(status)
             {
+                RenderPage(page);
+            }
+            page.setContent(doc.Content, null);
+        }
+        else
+        {
+            throw new SyntaxError("Some arguments are missing.");
+        }
+
+        function OnFinished()
+        {
+            system.stdout.writeLine(destination);
+            phantom.exit();
+        }
+
+        /**
+         * Replaces the pagenumber-variables inside the subject-variable.
+         * 
+         * @param subject
+         * The subject to check for pagenumber-variables.
+         */
+        function ReplacePageNumbers(subject : string, pageNumber : number, pageCount : number) : string
+        {
+            subject = subject.replace(/{{[\s]*(pageNumber|pageCount)[\s]*}}/g, function (match : string) : string
+            {
+                if (/pageNumber/g)
+                {
+                    return pageNumber.toString();
+                }
+                else if (/pageCount/g)
+                {
+                    return pageCount.toString();
+                }
+                return match;
+            });
+            return subject;
+        }
+
+        /**
+         * Renders a page.
+         * 
+         * @param page
+         * The page which is to be rendered.
+         */
+        function RenderPage(page)
+        {
+            page.paperSize = CalculatePaperSize(page);
+            page.render(destination, { type: 'pdf', quality: 75 });
+            OnFinished();
+        }
+
+        /**
+         * Calculates the paper-size of the document.
+         */
+        function CalculatePaperSize(page)
+        {
+            var paper = CreatePaper();
+            var styles = page.evaluate(GetStyles);
+
+            if (doc.Header || doc.SpecialHeaders.length > 0 || doc.EvenHeader || doc.OddHeader || doc.LastHeader)
+            {
+                paper.header = CreateHeader(styles);
+            }
+            if (doc.Footer || doc.SpecialFooters.length > 0 || doc.EvenFooter || doc.OddFooter || doc.LastFooter)
+            {
+                paper.footer = CreateFooter(styles);
+            }
+            return paper;
+        }
+
+        /**
+         * Creates a propper paper according to the document's Layout-settings.
+         */
+        function CreatePaper()
+        {
+            var paper : any = { };
+            var layout = doc.Layout;
+            
+            if (layout.Margin.Top || layout.Margin.Right || layout.Margin.Bottom || layout.Margin.Left)
+            {
+                paper.margin = {
+                    top: layout.Margin.Top,
+                    right: layout.Margin.Right,
+                    bottom: layout.Margin.Bottom,
+                    left: layout.Margin.Left
+                }
+            }
+            else
+            {
+                paper.margin = 0;
+            }
+
+            if (layout.Format)
+            {
+                paper.format = layout.Format;
+                paper.orientation = layout.Orientation;
+            }
+            else if (layout.Width && layout.Height)
+            {
+                paper.width = layout.Width;
+                paper.height = layout.Height;
+            }
+
+            return paper;
+        }
+
+        /**
+         * Returns the file-includes and styles of a DOM-document.
+         */
+        function GetStyles()
+        {
+            var styles = document.querySelectorAll('link,style');
+            styles = Array.prototype.reduce.call(styles, function (string, node) {
+                return string + (node.outerHTML || '')
+            }, '');
+            return styles;
+        }
+
+        /**
+         * Creates a header-section.
+         * 
+         * @param styles
+         * The css-styles of the header.
+         */
+        function CreateHeader(styles : string)
+        {
+            return {
+                height: doc.Header.Height,
+                contents: phantom.callback(function(pageNumber, pageCount)
+                {
+                    var header = GetHeader(pageNumber, pageCount);
+                    return styles + ReplacePageNumbers(header.Content, pageNumber, pageCount);
+                })
             }
         }
-        phantom.exit();
+
+        /**
+         * Determines the propper header according to the page-number and the number of pages.
+         * 
+         * @param pageNumber
+         * The page-number.
+         * 
+         * @param pageCount
+         * The number of pages.
+         */
+        function GetHeader(pageNumber, pageCount)
+        {
+            if (pageNumber == pageCount && doc.LastHeader)
+            {
+                return doc.LastHeader;
+            }
+            if (pageNumber in doc.SpecialHeaders)
+            {
+                return doc.SpecialHeaders[pageNumber];
+            }
+            else if (pageNumber % 2 == 0 && doc.EvenHeader)
+            {
+                return doc.EvenHeader;
+            }
+            else if (doc.OddHeader)
+            {
+                return doc.OddHeader;
+            }
+            else
+            {
+                return doc.Header;
+            }
+        }
+
+        /**
+         * Creates a footer-section.
+         * 
+         * @param styles
+         * The css-styles of the header.
+         */
+        function CreateFooter(styles : string)
+        {
+            return {
+                height: doc.Footer.Height,
+                contents: phantom.callback(function(pageNumber, pageCount)
+                {
+                    var footer = GetFooter(pageNumber, pageCount);
+                    return styles + ReplacePageNumbers(footer.Content, pageNumber, pageCount);
+                })
+            }
+        }
+
+        /**
+         * Determines the propper footer according to the page-number and the number of pages.
+         * 
+         * @param pageNumber
+         * The page-number.
+         * 
+         * @param pageCount
+         * The number of pages.
+         */
+        function GetFooter(pageNumber, pageCount)
+        {
+            if (pageNumber == pageCount && doc.LastFooter)
+            {
+                return doc.LastFooter;
+            }
+            if (pageNumber in doc.SpecialFooters)
+            {
+                return doc.SpecialFooters[pageNumber];
+            }
+            else if (pageNumber % 2 == 0 && doc.EvenFooter)
+            {
+                return doc.EvenFooter;
+            }
+            else if (doc.OddFooter)
+            {
+                return doc.OddFooter;
+            }
+            else
+            {
+                return doc.Footer;
+            }
+        }
     }
 }
