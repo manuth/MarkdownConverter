07611088315387d04eff6e046e02721faeb59b28
Manuel Thalmann <m@nuth.ch>
2017-03-27 02:08:28 +0200

    Adjusted the conversion-types and the package-manifest

--- src/ConversionType.ts
+++ src/ConversionType.ts
@@ -4,15 +4,47 @@
 export enum ConversionType
 {
     /**
+     * BMP-conversion
+     */
+    BMP,
+    /**
      * HTML-conversion.
      */
     HTML,
+
     /**
-     * DocX-conversion.
+     * JPG-conversion
      */
-    PNG,
+    JPEG,
     /**
      * PDF-conversion.
      */
-    PDF
+    PDF,
+
+    /**
+     * PNG-conversion.
+     */
+    PNG,
+    
+    /**
+     * PPM-conversion
+     */
+    PPM
+}
+
+/**
+ * The extensions of the conversion-types
+ */
+export let Extensions : { [id : number] : string } = BuildExtensions();
+
+function BuildExtensions() : { [id : number] : string }
+{
+    let extensions : { [id : number] : string } = { };
+    extensions[ConversionType.BMP] = '.bmp';
+    extensions[ConversionType.HTML] = '.html';
+    extensions[ConversionType.JPEG] = '.jpg';
+    extensions[ConversionType.PDF] = '.pdf';
+    extensions[ConversionType.PNG] = '.png';
+    extensions[ConversionType.PPM] = '.ppm';
+    return extensions;
 }
