c13c84365fe5bdf139d7cb810344927988d17e3c
Manuel Thalmann <m@nuth.ch>
2017-03-27 02:11:26 +0200

    Adjusted the extensions-file

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,13 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as Path from 'path';
+import * as url from 'url';
 import { DateTimeFormatter } from './Core/DateTimeFormatter';
 import { Document } from './Document';
 import { Footer, Header } from './Section';
 import { Converter } from "./Converter";
-import { ConversionType } from "./ConversionType";
+import { ConversionType, Extensions } from "./ConversionType";
+import { configKey } from "./Core/Constants";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -21,40 +23,84 @@ export function activate(context: vscode.ExtensionContext)
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('markdownConverter.convert', async () =>
+    let disposable = vscode.commands.registerCommand('markdownConverter.convert', () =>
     {
         // The code you place here will be executed every time your command is executed
-        // let markdownDoc = getMarkdownDoc();
 
-        // if (markdownDoc)
-        // {
-        //     let doc : Document;
-        //     if (markdownDoc.isUntitled)
-        //     {
-        //         doc = new Document();
-        //         doc.Content = markdownDoc.getText();
-        //     }
-        //     else
-        //     {
-        //         if (markdownDoc.isDirty)
-        //         {
-        //             markdownDoc.save();
-        //         }
-        //         doc = new Document(markdownDoc.fileName);
-        //     }
-        // }
-        // else
-        // {
-        //     throw Error('No markdown-file found.');
-        // }
-        let doc : Document = new Document();
-        doc.SpecialHeaders[3] = new Header('10cm', 'Seite 10');
-        doc.StyleSheets.push('../../styles.css');
-        doc.Content = '# Hello World\n{{ PageNumber }}\n{{ CreationDate }}';
+        let markdownDoc = getMarkdownDoc();
+        let config = vscode.workspace.getConfiguration(configKey);
+        let outDir = config.get<string>('outDir');
 
-        var x = new Converter(doc);
-        x.Start(ConversionType.PDF, Path.join(__dirname, '..', '..', 'test.pdf'));
-        console.log(doc.toJSON());
+        if (vscode.workspace.rootPath)
+        {
+            process.chdir(vscode.workspace.rootPath);
+        }
+        else if (outDir)
+        {
+            process.chdir(outDir);
+        }
+
+        if (markdownDoc)
+        {
+            let type = config.get('conversionType');
+            let name = config.get<string>('document.name');
+            let types : string[] = [ ];
+            let doc : Document;
+
+            if (!name)
+            {
+                if (!markdownDoc.isUntitled)
+                {
+                    name = Path.parse(markdownDoc.fileName).name;
+                }
+                else
+                {
+                    name = 'temp';
+                }
+            }
+
+            if ((typeof type == 'string'))
+            {
+                types.push(type);
+            }
+            else
+            {
+                types = (type as string[]);
+            }
+
+            if (!markdownDoc.isUntitled)
+            {
+                if (!Path.isAbsolute(outDir))
+                {
+                    outDir = Path.join(Path.dirname(markdownDoc.fileName), outDir);
+                }
+            }
+
+            if (markdownDoc.isUntitled || (markdownDoc.isDirty && !config.autoSave))
+            {
+                doc = new Document();
+                doc.Content = markdownDoc.getText();
+            }
+            else
+            {
+                if (markdownDoc.isDirty)
+                {
+                    markdownDoc.save();
+                }
+                doc = new Document(markdownDoc.fileName);
+            }
+            
+            let converter = new Converter(doc);
+
+            types.forEach(type => {
+                let destination = Path.join(outDir, name + Extensions[type]);
+                converter.Start(ConversionType[type], destination);
+            });
+        }
+        else
+        {
+            throw Error('No markdown-file found.');
+        }
     });
 
     context.subscriptions.push(disposable);
@@ -64,7 +110,8 @@ export function activate(context: vscode.ExtensionContext)
      */
     function getMarkdownDoc() : vscode.TextDocument
     {
-        if (vscode.window.activeTextEditor.document.languageId == 'markdown')
+        let config = vscode.workspace.getConfiguration(configKey);
+        if (config.has('ignoreLanguage') && config.get<boolean>('ignoreLanguage') && vscode.window.activeTextEditor || vscode.window.activeTextEditor.document.languageId == 'markdown')
         {
             return vscode.window.activeTextEditor.document;
         }
