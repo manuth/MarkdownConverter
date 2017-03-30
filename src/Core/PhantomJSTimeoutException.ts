5886ed92294f8921f32f7c97466dd7c2e03492db
Manuel Thalmann <m@nuth.ch>
2017-03-30 19:45:10 +0200

    Improve error-handling

---
+++ src/Core/PhantomJSTimeoutException.ts
@@ -0,0 +1,8 @@
+import { TimeoutException } from "./TimeoutException";
+
+/**
+ * Represents a Timeout-exception triggered by PhantomJS
+ */
+export class PhantomJSTimeoutException extends TimeoutException
+{
+}
