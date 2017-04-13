50b559155a692efd405c8c7dbc8c962fd8c38eaa
Manuel Thalmann <m@nuth.ch>
2017-04-13 17:50:28 +0200

    Add TypeScript-types

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/mustache/index.d.ts" />
+/// <reference path="modules/markdown-it/index.d.ts" />
+/// <reference path="modules/mkdirp/index.d.ts" />
+/// <reference path="modules/npm/index.d.ts" />
+/// <reference path="modules/shelljs/index.d.ts" />
+/// <reference path="modules/temp/index.d.ts" />
