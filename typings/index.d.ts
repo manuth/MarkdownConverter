b4fd63d09cc320bcecdb6c710096ab41e12e15c3
Manuel Thalmann <m@nuth.ch>
2017-08-24 16:19:39 +0200

    Uninstall typings and install @types instead

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/mustache/index.d.ts" />
-/// <reference path="modules/markdown-it/index.d.ts" />
-/// <reference path="modules/mkdirp/index.d.ts" />
-/// <reference path="modules/npm/index.d.ts" />
-/// <reference path="modules/shelljs/index.d.ts" />
-/// <reference path="modules/temp/index.d.ts" />
