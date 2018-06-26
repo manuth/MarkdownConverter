76707ac8c179235d136ea9813b4347a78d493d70
Manuel Thalmann <m@nuth.ch>
2018-06-26 09:53:42 +0200

    Remove "use strict"-remarks

--- src/Properties/Settings.ts
+++ src/Properties/Settings.ts
@@ -1,4 +1,3 @@
-'use strict';
 import * as VSCode from 'vscode';
 import { ConversionType } from "../ConversionType";
 import { EmbeddingOption } from "../System/Drawing/EmbeddingOption";
--- src/System/Exception.ts
+++ src/System/Exception.ts
@@ -1,4 +1,3 @@
-'use strict';
 /**
  * Represents an Exception
  */
