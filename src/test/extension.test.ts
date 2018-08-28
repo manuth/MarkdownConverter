888abbf9dd1df6a754ee8e2218ab11946f6fcb42
Manuel Thalmann <m@nuth.ch>
2018-08-29 00:38:05 +0200

    Add unit-tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,30 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 import * as assert from "assert";
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-import * as myExtension from "../extension";
+import * as Dedent from "dedent";
+import * as MarkdownIt from "markdown-it";
+import { Utilities } from "../System/Utilities";
 
 // Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+suite("MarkdownConverter", () =>
+{
+    /**
+     * Clones the parser provided by VSCode and verifies whether mutations affect VSCode's parser.
+     */
+    test("Clone VSCodeParser", () =>
+    {
+        let text = "<b>Hello World</b>";
+        let code = Dedent(`
+            \`\`\`cs
+            Console.WriteLine("Hello World");
+            \`\`\``);
+        let codeResult = "<pre>cs</pre>";
+        Utilities.VSCodeParser = new MarkdownIt({ highlight: (subjectr, language) => `<pre>${language}</pre>` });
+        let parser1 = Utilities.VSCodeParser;
+        parser1.set({ html: false });
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+        let parser2 = Utilities.VSCodeParser;
+        parser2.set({ html: true });
+        assert.equal(parser1.render(code).trim(), codeResult);
+        assert.equal(parser2.render(code).trim(), codeResult);
+        assert.notEqual(parser1.render(text), parser2.render(text));
     });
 });
