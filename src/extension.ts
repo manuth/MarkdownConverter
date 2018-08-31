448717fb8010848269c82999e33afb54028a0eb8
Manuel Thalmann <m@nuth.ch>
2018-08-31 23:29:36 +0200

    Rework the way to convert documents

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,10 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import CultureInfo from "culture-info";
-import * as FileSystem from "fs-extra";
 import { MarkdownIt } from "markdown-it";
-import * as Path from "path";
-import * as Puppeteer from "puppeteer";
-import * as Format from "string-template";
-import { commands, env, ExtensionContext, ProgressLocation, TextDocument, window, workspace } from "vscode";
+import { commands, env, ExtensionContext } from "vscode";
 import { ConvertCommand } from "./MarkdownConverter/ConvertCommand";
-import { getMarkdownExtensionContributions } from "./MarkdownConverter/MarkdownExtensions";
-import { MarkdownFileNotFoundException } from "./MarkdownConverter/MarkdownFileNotFoundException";
-import { Program } from "./Program";
 import { ResourceManager } from "./Properties/ResourceManager";
-import { Settings } from "./Properties/Settings";
-import { Exception } from "./System/Exception";
-
-let markdown;
 
 /**
  * Represens the extension itself.
@@ -30,7 +19,7 @@ export class Extension
     /**
      * The parser provided by `Visual Studio Code`
      */
-    private vsCodeParser: MarkdownIt;
+    private vsCodeParser: MarkdownIt = null;
 
     /**
      * Initializes a new instance of the `Extension` class.
@@ -66,154 +55,9 @@ export class Extension
     {
         this.context = context;
 
-        // context.subscriptions.push(
-        //     commands.registerCommand("markdownConverter.Convert", this.Convert)
-        // );
-        let disposables = [
-            commands.registerCommand("markdownConverter.Convert", async () =>
-            {
-                if (await FileSystem.pathExists(Puppeteer.executablePath()))
-                {
-                    try
-                    {
-                        let document = getMarkdownDoc();
-    
-                        /* Preparing the arguments */
-                        let documentRoot: string;
-                        let outDir = Settings.Default.OutputDirectory;
-    
-                        let currentWorkspace = (workspace.workspaceFolders || []).find(
-                            (workspaceFolder) =>
-                            {
-                                let workspaceParts = workspaceFolder.uri.fsPath.split(Path.sep);
-                                let documentParts = document.uri.fsPath.split(Path.sep);
-    
-                                return workspaceParts.every(
-                                    (value, index) =>
-                                    {
-                                        return value === documentParts[index];
-                                    });
-                            });
-    
-                        if (currentWorkspace)
-                        {
-                            documentRoot = currentWorkspace.uri.fsPath;
-                        }
-                        else if (!document.isUntitled)
-                        {
-                            documentRoot = Path.dirname(document.fileName);
-                        }
-                        else
-                        {
-                            documentRoot = process.cwd();
-                        }
-    
-                        if (!Path.isAbsolute(outDir))
-                        {
-                            outDir = Path.resolve(documentRoot, outDir);
-                        }
-    
-                        await Program.Main(documentRoot, document, Settings.Default.ConversionType, outDir, markdown, getMarkdownExtensionContributions(context));
-                    }
-                    catch (e)
-                    {
-                        let message;
-    
-                        if (e instanceof Exception)
-                        {
-                            message = e.Message;
-                        }
-                        else
-                        {
-                            throw e;
-                        }
-    
-                        window.showErrorMessage(message);
-                    }
-                }
-                else if (
-                    await window.showInformationMessage(
-                        ResourceManager.Resources.Get("UpdateMessage"),
-                        ResourceManager.Resources.Get<string>("Yes"),
-                        ResourceManager.Resources.Get<string>("No")) === ResourceManager.Resources.Get("Yes"))
-                {
-                    let revision = require(Path.join("..", "node_modules", "puppeteer", "package.json")).puppeteer.chromium_revision;
-                    let promptRetry;
-    
-                    do
-                    {
-                        promptRetry = false;
-    
-                        await window.withProgress(
-                            {
-                                location: ProgressLocation.Notification,
-                                title: Format(ResourceManager.Resources.Get("UpdateRunning"), revision)
-                            }, async (reporter) =>
-                            {
-                                try
-                                {
-                                    let progress = 0;
-                                    let browserFetcher = (Puppeteer as any).createBrowserFetcher();
-                                    await browserFetcher.download(
-                                        revision,
-                                        (downloadedBytes, totalBytes) =>
-                                        {
-                                            let newProgress = Math.floor((downloadedBytes / totalBytes) * 100);
-    
-                                            if (newProgress > progress)
-                                            {
-                                                reporter.report({
-                                                    increment: newProgress - progress
-                                                });
-    
-                                                progress = newProgress;
-                                            }
-                                        });
-    
-                                    window.showInformationMessage(ResourceManager.Resources.Get("UpdateSuccess"));
-                                }
-                                catch
-                                {
-                                    promptRetry = true;
-                                }
-                            });
-                    }
-                    while (
-                        !await FileSystem.pathExists(Puppeteer.executablePath()) &&
-                        promptRetry &&
-                        await window.showWarningMessage(
-                            ResourceManager.Resources.Get("UpdateFailed"),
-                            ResourceManager.Resources.Get("Yes"),
-                            ResourceManager.Resources.Get("No")) === ResourceManager.Resources.Get("Yes"));
-                }
-            })
-        ];
-    
-        for (let disposable of disposables)
-        {
-            context.subscriptions.push(disposable);
-        }
-    
-        /**
-         * Tries to find a markdown-file
-         */
-        function getMarkdownDoc(): TextDocument
-        {
-            if (window.activeTextEditor && (window.activeTextEditor.document.languageId === "markdown" || Settings.Default.IgnoreLanguageMode))
-            {
-                return window.activeTextEditor.document;
-            }
-            for (let textEditor of window.visibleTextEditors)
-            {
-                if (textEditor.document.languageId === "markdown")
-                {
-                    return textEditor.document;
-                }
-            }
-    
-            throw new MarkdownFileNotFoundException();
-        }
-    
+        context.subscriptions.push(
+            commands.registerCommand("markdownConverter.Convert", async () => await this.Convert()));
+
         return {
             extendMarkdownIt: (md: any) =>
             {
@@ -224,11 +68,11 @@ export class Extension
     }
 
     /**
-     * Converts the current document.
+     * Converts an opened `TextDocument`.
      */
-    public async Convert()
+    protected async Convert()
     {
-        new ConvertCommand(this).Execute();
+        await new ConvertCommand(this).Execute();
     }
 
     /**
