b109c9bf6c7fa25576b301d2a5a74242a7cec426
Manuel Thalmann <m@nuth.ch>
2019-09-03 23:01:50 +0200

    Add tests for the DocumentFragment class

---
+++ MarkdownConverter/src/tests/System/Documents/DocumentFragment.test.ts
@@ -0,0 +1,66 @@
+import Assert = require("assert");
+import MarkdownIt = require("markdown-it");
+import { TextDocument, workspace } from "vscode";
+import { Document } from "../../../System/Documents/Document";
+import { DocumentFragment } from "../../../System/Documents/DocumentFragment";
+
+suite(
+    "DocumentFragment",
+    () =>
+    {
+        let text: string;
+        let verifier: string;
+        let textDocument: TextDocument;
+        let document: Document;
+
+        suiteSetup(
+            async () =>
+            {
+                text = "hello world";
+                verifier = "rendered by document: ";
+                textDocument = await workspace.openTextDocument({ language: "markdown" });
+
+                document = new class extends Document
+                {
+                    constructor()
+                    {
+                        super(textDocument, new MarkdownIt());
+                    }
+
+                    /**
+                     * @inheritdoc
+                     */
+                    protected async RenderText(text: string)
+                    {
+                        return verifier + text;
+                    }
+                }();
+            });
+
+        suite(
+            "constructor(Document document)",
+            () =>
+            {
+                test(
+                    "Checking whether the properties are set correctly…",
+                    () =>
+                    {
+                        let documentFragment = new DocumentFragment(document);
+                        Assert.strictEqual(documentFragment.Document, document);
+                    });
+            });
+
+        suite(
+            "RenderText(string text)",
+            () =>
+            {
+                test(
+                    "Checking whether the text is rendered by the `Document`-object…",
+                    async () =>
+                    {
+                        let documentFragment = new DocumentFragment(document);
+                        documentFragment.Content = text;
+                        Assert.strictEqual(await documentFragment.Render(), verifier + text);
+                    });
+            });
+    });
--- MarkdownConverter/src/tests/System/Documents/index.ts
+++ MarkdownConverter/src/tests/System/Documents/index.ts
@@ -3,5 +3,6 @@ suite(
     () =>
     {
         require("./Renderable.test");
+        require("./DocumentFragment.test");
         require("./CustomPaperFormat.test");
     });
