937c14a60e319033211901fc6d00a5e49141046c
Manuel Thalmann <m@nuth.ch>
2019-09-12 09:01:15 +0200

    Ensure the system-parser is disabled during tests

--- MarkdownConverter/src/test/single-file.test.ts
+++ MarkdownConverter/src/test/single-file.test.ts
@@ -1,6 +1,33 @@
+import { ConfigurationTarget, workspace, WorkspaceConfiguration } from "vscode";
+import { Settings } from "../Properties/Settings";
+import { ConfigRestorer } from "../tests/ConfigRestorer";
+
 suite(
     "Test for Visual Studio Code in Single-File Mode",
     () =>
     {
+        let config: WorkspaceConfiguration;
+        let configRestorer: ConfigRestorer;
+
+        suiteSetup(
+            async () =>
+            {
+                config = workspace.getConfiguration(Settings["configKey"]);
+                configRestorer = new ConfigRestorer(
+                    [
+                        "Parser.SystemParserEnabled"
+                    ],
+                    Settings["configKey"]);
+
+                await configRestorer.Clear();
+                await config.update("Parser.SystemParserEnabled", false, ConfigurationTarget.Global);
+            });
+
+        suiteTeardown(
+            async () =>
+            {
+                await configRestorer.Restore();
+            });
+
         require("../tests/single-file/main.test");
     });
