38ddb3f3481292b302a583ee61898b4a1f2d8896
Manuel Thalmann <m@nuth.ch>
2021-09-15 22:51:44 +0200

    Add a localized message for the `FileNotFoundException`

--- src/System/IO/FileNotFoundException.ts
+++ src/System/IO/FileNotFoundException.ts
@@ -11,20 +11,19 @@ export class FileNotFoundException extends FileException
     /**
      * Initializes a new instance of the {@link FileNotFoundException `FileNotFoundException`} class.
      *
-     * @param message
-     * Either the error message that explains the reason for the exception or `null` to use the default message.
-     *
      * @param path
      * The path to the file which caused the exception.
+     *
+     * @param message
+     * Either the error message that explains the reason for the exception or `null` to use the default message.
      *
      * @param innerException
      * The exception that is the cause of the current exception, or `null` if no inner exception is specified.
      */
-    public constructor(message: string, path: string, innerException?: Exception)
+    public constructor(path: string, message?: string, innerException?: Exception)
     {
         if (arguments.length === 1)
         {
-            path = message;
             message = format(Resources.Resources.Get("FileNotFoundException"), path);
         }
 
