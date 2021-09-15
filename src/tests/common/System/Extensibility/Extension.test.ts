6eacdf6ea665e86427b432842096965742d9501e
Manuel Thalmann <m@nuth.ch>
2021-09-15 21:42:53 +0200

    Refactor the `Extension`-tests

--- src/tests/common/System/Extensibility/Extension.test.ts
+++ src/tests/common/System/Extensibility/Extension.test.ts
@@ -1,6 +1,13 @@
-import { strictEqual } from "assert";
+import { doesNotReject, notStrictEqual, ok, strictEqual } from "assert";
 import { Package } from "@manuth/package-json-editor";
+import { CultureInfo } from "@manuth/resource-manager";
+import MarkdownIt = require("markdown-it");
+import { Random } from "random-js";
+import { createSandbox, SinonSandbox } from "sinon";
+import { commands, env, window, workspace } from "vscode";
+import { Resources } from "../../../../Properties/Resources";
 import { Extension } from "../../../../System/Extensibility/Extension";
+import { Task } from "../../../../System/Tasks/Task";
 import { TestConstants } from "../../../TestConstants";
 
 /**
@@ -12,18 +19,69 @@ export function ExtensionTests(): void
         nameof(Extension),
         () =>
         {
-            let extension: Extension;
+            let random: Random;
+            let sandbox: SinonSandbox;
+            let extension: TestExtension;
             let npmPackage: Package;
+
+            /**
+             * Provides an implementation of the {@link Extension `Extension`} class for testing.
+             */
+            class TestExtension extends Extension
+            {
+                /**
+                 * @inheritdoc
+                 *
+                 * @param task
+                 * The task to execute.
+                 */
+                public override async ExecuteTask(task: Task): Promise<void>
+                {
+                    return super.ExecuteTask(task);
+                }
+            }
+
+            suiteSetup(
+                () =>
+                {
+                    random = new Random();
+                });
 
             setup(
                 () =>
                 {
+                    sandbox = createSandbox();
                     npmPackage = TestConstants.PackageMetadata;
-                    extension = new Extension(npmPackage);
+                    extension = new TestExtension(npmPackage);
+                });
+
+            teardown(
+                () =>
+                {
+                    sandbox.restore();
                 });
 
             suite(
-                nameof<Extension>((extension) => extension.Author),
+                nameof(Extension.constructor),
+                () =>
+                {
+                    test(
+                        `Checking whether the ${nameof(Resources)}\`' \`${nameof(Resources.Culture)}\` is set to vscode's language…`,
+                        () =>
+                        {
+                            Resources.Culture = CultureInfo.InvariantCulture;
+                            new Extension(npmPackage);
+
+                            for (let resource of [Resources.Resources, Resources.Files])
+                            {
+                                notStrictEqual(resource.Locale, CultureInfo.InvariantCulture);
+                                strictEqual(resource.Locale.Name, new CultureInfo(env.language).Name);
+                            }
+                        });
+                });
+
+            suite(
+                nameof<TestExtension>((extension) => extension.Author),
                 () =>
                 {
                     test(
@@ -35,7 +93,7 @@ export function ExtensionTests(): void
                 });
 
             suite(
-                nameof<Extension>((extension) => extension.Name),
+                nameof<TestExtension>((extension) => extension.Name),
                 () =>
                 {
                     test(
@@ -47,7 +105,7 @@ export function ExtensionTests(): void
                 });
 
             suite(
-                nameof<Extension>((extension) => extension.FullName),
+                nameof<TestExtension>((extension) => extension.FullName),
                 () =>
                 {
                     test(
@@ -55,6 +113,88 @@ export function ExtensionTests(): void
                         () =>
                         {
                             strictEqual(extension.FullName, `${extension.Author}.${extension.Name}`);
+                        });
+                });
+
+            suite(
+                nameof<TestExtension>((extension) => extension.Activate),
+                () =>
+                {
+                    let extensionMethod = "extendMarkdownIt";
+
+                    setup(
+                        () =>
+                        {
+                            sandbox.replace(window, "showTextDocument", () => null);
+                            sandbox.replace(workspace, "openTextDocument", () => null);
+                            sandbox.replace(commands, "executeCommand", () => null);
+                        });
+
+                    test(
+                        `Checking whether the \`${extensionMethod}\`-method exposed by the \`${nameof<TestExtension>((e) => e.Activate)}\`-method resolves the system-parser fix \`${nameof(Promise)}\`…`,
+                        async () =>
+                        {
+                            let parser = new MarkdownIt();
+                            let systemParserFixPromise = extension.EnableSystemParser();
+                            (await extension.Activate() as any)[extensionMethod](parser);
+                            doesNotReject(() => systemParserFixPromise);
+                            strictEqual(extension.VSCodeParser, parser);
+                        });
+                });
+
+            suite(
+                nameof<TestExtension>((extension) => extension.ExecuteTask),
+                () =>
+                {
+                    let messages: string[];
+                    let error: Error;
+
+                    setup(
+                        () =>
+                        {
+                            messages = [];
+                            error = new Error(random.string(20));
+
+                            sandbox.replace(
+                                window,
+                                "showErrorMessage",
+                                async (message: string) =>
+                                {
+                                    messages.push(message);
+                                    return "";
+                                });
+                        });
+
+                    test(
+                        "Checking whether an error-message is displayed if the task throws an exception…",
+                        async () =>
+                        {
+                            await extension.ExecuteTask(
+                                new class extends Task
+                                {
+                                    /**
+                                     * @inheritdoc
+                                     */
+                                    public get Title(): string
+                                    {
+                                        return "Test";
+                                    }
+
+                                    /**
+                                     * @inheritdoc
+                                     */
+                                    public async Execute(): Promise<void>
+                                    {
+                                        throw error;
+                                    }
+                                }(extension));
+
+                            ok(
+                                messages.some(
+                                    (message) =>
+                                    {
+                                        return message.includes(error.message);
+                                    }));
                         });
                 });
         });
