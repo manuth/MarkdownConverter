c84ad206765c79f331ad534fd95baaa317d67097
Manuel Thalmann <m@nuth.ch>
2021-09-19 18:47:09 +0200

    Add missing paper-formats

--- src/System/Documents/StandardizedFormatType.ts
+++ src/System/Documents/StandardizedFormatType.ts
@@ -4,6 +4,21 @@
 export enum StandardizedFormatType
 {
     /**
+     * Indicates the A0-format.
+     */
+    A0 = "a0",
+
+    /**
+     * Indicates the A1-format.
+     */
+    A1 = "a1",
+
+    /**
+     * Indicates the A2-format.
+     */
+    A2 = "a2",
+
+    /**
      * Indicates the A3-format.
      */
     A3 = "a3",
@@ -19,6 +34,11 @@ export enum StandardizedFormatType
     A5 = "a5",
 
     /**
+     * Indicates the A6-format.
+     */
+    A6 = "a6",
+
+    /**
      * Indicates the Legal-format.
      */
     Legal = "legal",
@@ -31,5 +51,10 @@ export enum StandardizedFormatType
     /**
      * Indicates the Tabloid-format.
      */
-    Tabloid = "tabloid"
+    Tabloid = "tabloid",
+
+    /**
+     * Indicates the ledger-format.
+     */
+    Ledger = "ledger"
 }
